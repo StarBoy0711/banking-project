2d6425f29f19f4d41852da6f5075b062865333a6
Darshan Jain <jaindarshan295@gmail.com>
2022-10-12 06:37:18 +0530

    final2

--- js.js
+++ js.js
@@ -1,12 +1,3 @@
-// const labels = document.querySelectorAll('.form-control label')
-
-// labels.forEach(label => {
-//     label.innerHTML = label.innerText
-//     .split('')
-//     .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
-//     .join('')
-// })  
-
 const formlog = document.getElementById("login-form");
 const BtnLog = document.getElementById("login-form-submit");
 const ErrorMessage = document.getElementById("login-error-msg");
